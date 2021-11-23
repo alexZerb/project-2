7d733bc53f7c6ea4c1ed734db0b467479058b701
Alex Szczerba <Szczerba18@icloud.com>
2021-11-22 23:21:58 -0700

    showpage
    work on showpage

--- js/script.js
+++ js/script.js
@@ -20,18 +20,36 @@ This function will create and insert/append the elements needed to display a "pa
 
 function showpage(list, page) {
  // create two variables which will represent the index for the first and last student on the page
- const firstStudent = (page * 9) - 9;
- const lastStudent = page * 9;
+ const startIndex = page * 9 - 9;
+ const endIndex = page * 9;
   // select the element with a class of `student-list` and assign it to a variable
- const studentList = document.querySelector('ul');
+ const studentList = document.querySelector('ul.student-list');
   // set the innerHTML property of the variable you just created to an empty string
-
+ studentList.innerHtml = '';
+ let studentItem = '';
   // loop over the length of the `list` parameter
-    // inside the loop create a conditional to display the proper students
+for(let i = 0; i < list.length; i++) {
+  // inside the loop create a conditional to display the proper students
+    if(i >= startIndex && i < endIndex) {  
       // inside the conditional:
         // create the elements needed to display the student information
         // insert the above elements
-
+      studentItem += `
+      <li class="student-item cf">
+      <div class="student-details">
+        <img class="avatar" src=${list[i].picture.thumbnail} alt="Profile Picture">
+        <h3>${list[i].name.first}</h3>
+        <span class="email">${list[i].email}</span>
+      </div>
+      <div class="joined-details">
+        <span class="date">Joined ${list[i].registered.date}</span>
+      </div>
+    </li>
+      `;
+      
+     return studentList.insertAdjacentHTML("beforeend", studentItem);   
+     }
+   };    
 }
 
 /*
@@ -42,3 +60,4 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
+showpage(data, 1);
