a8938db2d1f4800117f9ae91e68d3a9d7e5e008a
Alex Szczerba <Szczerba18@icloud.com>
2021-12-03 15:30:12 -0700

    showpage function
    refactoring function

--- js/script.js
+++ js/script.js
@@ -20,8 +20,8 @@ This function will create and insert/append the elements needed to display a "pa
 
 function showpage(list, page) {
  // create two variables which will represent the index for the first and last student on the page
- const startIndex = page * 9 - 9;
- const endIndex = page * 9;
+ let startIndex = page * 9 - 9;
+ let endIndex = page * 9;
   // select the element with a class of `student-list` and assign it to a variable
  const studentList = document.querySelector('ul.student-list');
   // set the innerHTML property of the variable you just created to an empty string
@@ -29,11 +29,14 @@ function showpage(list, page) {
  let studentItem = '';
   // loop over the length of the `list` parameter
 for(let i = 0; i < list.length; i++) {
+   let currentPage = list[i];
   // inside the loop create a conditional to display the proper students
-    if(i >= startIndex && i < endIndex) {  
+    if(currentPage >= list.indexOf(currentPage) 
+      && list.indexOf(currentPage) < endIndex) {  
       // inside the conditional:
         // create the elements needed to display the student information
         // insert the above elements
+      
       studentItem += `
       <li class="student-item cf">
       <div class="student-details">
@@ -46,10 +49,10 @@ for(let i = 0; i < list.length; i++) {
       </div>
     </li>
       `;
-      
+      console.log(studentItem);
      return studentList.insertAdjacentHTML("beforeend", studentItem);   
      }
-   };    
+   }    
 }
 
 /*
