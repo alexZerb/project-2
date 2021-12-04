a80d3d9676f3b645a63a3a34519776eb2640bcd3
Alex Szczerba <Szczerba18@icloud.com>
2021-12-04 05:33:06 -0700

    showpage function
    function correctly creates list items for 9 students

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 console.log(data);
 
-const studentPerPage = 8;
+const studentPerPage = 9;
 
 
 
@@ -12,21 +12,38 @@ const studentPerPage = 8;
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-function showPage(page) {
+function showPage(list, page) {
  // create two variables which will represent the index for the first and last student on the page
  let startIndex = page * studentPerPage - studentPerPage;
  let endIndex = page * studentPerPage;
   // select the element with a class of `student-list` and assign it to a variable
   // set the innerHTML property of the variable you just created to an empty string
  const studentList = document.querySelector('.student-list');
- console.log(studentList);
+ studentList.innerHTML = '';
   // loop over the length of the `list` parameter
-
-  // inside the loop create a conditional to display the proper students
-   
+ for (let i = 0; i < list.length; i++) {
+   let currentStudents = list[i];
+    
+ // inside the loop create a conditional to display the proper students
+   if (list.indexOf(currentStudents) >= startIndex && list.indexOf(currentStudents) < endIndex) {
+      const li = document.createElement('li'); 
+      li.className = 'student-item cf'
+      li.innerHTML = `
+        <div class="student-details">
+          <img class="avatar" src=${currentStudents.picture.large} alt="Profile Picture">
+          <h3>${currentStudents.name.title} ${currentStudents.name.first} ${currentStudents.name.last}</h3>
+          <span class="email">${currentStudents.email}</span>
+        </div>
+        <div class="joined-details">
+          <span class="date">Joined ${currentStudents.registered.date}</span>
+        </div>
+      `;
+      studentList.append(li);
       // inside the conditional:
         // create the elements needed to display the student information
         // insert the above elements
+   } 
+ }
 }
 
 /*
@@ -37,4 +54,4 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
-showPage(1);
+showPage(data, 1);
