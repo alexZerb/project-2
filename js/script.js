a73eb4a80634d3ebae4f96087571847ca98e0505
Alex Szczerba <Szczerba18@icloud.com>
2021-12-04 20:43:57 -0700

    pagination function
    started writing function to add buttons to select pages

--- js/script.js
+++ js/script.js
@@ -4,27 +4,21 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 console.log(data);
 
-const studentPerPage = 9;
-
-
+const itemsPerPage = 9;
 
 /*
 Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
+This function creatse and inserts/appends the elements needed to display a "page" of nine students
 */
 function showPage(list, page) {
- // create two variables which will represent the index for the first and last student on the page
- let startIndex = page * studentPerPage - studentPerPage;
- let endIndex = page * studentPerPage;
-  // select the element with a class of `student-list` and assign it to a variable
-  // set the innerHTML property of the variable you just created to an empty string
+ let startIndex = page * itemsPerPage - itemsPerPage;
+ let endIndex = page * itemsPerPage;
  const studentList = document.querySelector('.student-list');
  studentList.innerHTML = '';
-  // loop over the length of the `list` parameter
+  
  for (let i = 0; i < list.length; i++) {
    let currentStudents = list[i];
     
- // inside the loop create a conditional to display the proper students
    if (list.indexOf(currentStudents) >= startIndex && list.indexOf(currentStudents) < endIndex) {
       const li = document.createElement('li'); 
       li.className = 'student-item cf'
@@ -39,9 +33,6 @@ function showPage(list, page) {
         </div>
       `;
       studentList.append(li);
-      // inside the conditional:
-        // create the elements needed to display the student information
-        // insert the above elements
    } 
  }
 }
@@ -51,6 +42,32 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination(list) {
+
+ // create a variable to calculate the number of pages needed
+
+  const numOfPages = Math.ceil(list.length / itemsPerPage);
+
+  // select the element with a class of `link-list` and assign it to a variable
+ 
+  const linkList = document.querySelector('.link-list');
+ 
+  // set the innerHTML property of the variable you just created to an empty string
+ 
+  linkList.innerHTML = '';
+ 
+  // loop over the number of pages needed
+    // create the elements needed to display the pagination button
+    // insert the above elements
+
+  // give the first pagination button a class of "active"
+
+  // create an event listener on the `link-list` element
+    // if the click target is a button:
+      // remove the "active" class from the previous button
+      // add the active class to the clicked button
+      // call the showPage function passing the `list` parameter and page to display as arguments
+}   
 
 
 // Call functions
